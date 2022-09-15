bce75fc9f5808a38e806b544c9c0f9daac8a7e06
I-GiM <ikennaoyiih@gmail.com>
2022-09-14 19:07:32 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - The main program
+ *
+ * Return: function returns zero as value
+*/
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)");
+	printf("Size of an int: 4 byte(s)");
+	printf("Size of a long int: 4 byte(s)");
+	printf("Size of a long long int: 8 byte(s)");
+	printf("Size of a float: 4 byte(s)");
+	return (0);
+}
