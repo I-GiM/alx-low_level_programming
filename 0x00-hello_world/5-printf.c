3b8354566ca6a028942d894b986c58e877f3afbc
I-GiM <ikennaoyiih@gmail.com>
2022-09-14 18:50:54 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - This is the body of the code
+ *
+ * Return: Must return an integer value always zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
