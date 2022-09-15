b0eaac5f5d6e1ca46db933c15629b9740ffbf00a
I-GiM <ikennaoyiih@gmail.com>
2022-09-14 18:08:26 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
-/* A function that prints a string */
+/**
+ * main - This is the body of the code
+ *
+ * Return: Must return an integer value always zero
+ */
 int main(void)
 {
-	/* Function that prints a string with puys function */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
