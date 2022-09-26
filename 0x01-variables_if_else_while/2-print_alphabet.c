5d539ce3421969910e7ee85a79d63a7722563d1f
I-GiM <ikennaoyiih@gmail.com>
2022-09-26 12:24:23 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in uppercase
+ * Description:
+ * Return: 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		putchar(i);
+		putchar('\n');
+	}
+}
