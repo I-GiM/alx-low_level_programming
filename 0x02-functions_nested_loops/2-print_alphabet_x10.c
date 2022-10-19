f029afc99cb5dcfb5af7983e63720b29f266b552
I-GiM <ikennaoyiih@gmail.com>
2022-10-19 08:20:45 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,6 @@
 
 /**
  * print_alphabet_x10 - check the code.
- * 
  */
 
 void print_alphabet_x10(void)
@@ -14,6 +13,7 @@ void print_alphabet_x10(void)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
+		_putchar('\n');
 		count++;
 	}
 }
