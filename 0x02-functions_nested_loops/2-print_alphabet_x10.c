a5e810e3c4520ced03f56a7af0c9a1c757dbafbe
I-GiM <ikennaoyiih@gmail.com>
2022-10-19 08:18:22 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * print_alphabet_x10 - check the code.
  * 
- * Return: Always 0.
  */
 
 void print_alphabet_x10(void)
@@ -18,8 +17,4 @@ void print_alphabet_x10(void)
 		count++;
 	}
 }
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
-}
+
